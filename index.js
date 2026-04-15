058d553ae895aa59d0be2ede8c0b22eb1a4a4da2
Raul Berez <raulberez@users.noreply.github.com>
2026-04-15 15:00:48 +0800

    export a default logger instance for quick use

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { logFactory } = require('./src/log.js');
 
+// convenience shorthand so I don't have to destructure every time
+const logger = logFactory();
+
 module.exports = {
-	logFactory
+	logFactory,
+	logger
 }
