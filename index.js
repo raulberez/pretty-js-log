3824696c36fc22560a6d74ccaded3f3aefce4173
Raul Berez <raulberez@users.noreply.github.com>
2026-04-16 07:55:07 +0800

    add a pre-configured debug logger export for convenience

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const { logFactory } = require('./src/log.js');
 // convenience shorthand so I don't have to destructure every time
 const logger = logFactory();
 
+// also export a pre-configured debug logger for development
+const debugLogger = logFactory({ level: 'debug' });
+
 module.exports = {
 	logFactory,
-	logger
+	logger,
+	debugLogger
 }
